86b439bd8315ce4350c112437472e347312ec587
Ihsan Demir <ihsandemir.y@gmail.com>
2023-05-07 23:49:46 -0400

    Page components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DonorComponent } from './pages/donor/donor.component';
+import { IncomeComponent } from './pages/income/income.component';
+import { ExpenseComponent } from './pages/expense/expense.component';
+import { ReportComponent } from './pages/report/report.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonorComponent,
+    IncomeComponent,
+    ExpenseComponent,
+    ReportComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
